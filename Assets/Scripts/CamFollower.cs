f92c647dd47a58c0b576bb5174dd16c9cb7eca4c
kasradzenika <kasradzenika@yahoo.com>
2017-04-20 20:52:13 +0400

    Commit - Lecture 1

---
+++ Assets/Scripts/CamFollower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollower : MonoBehaviour
+{
+    public Transform target;
+    Vector3 distanceFromTarget;
+
+	void Start ()
+    {
+        distanceFromTarget = transform.position - target.position;
+    }
+	
+	void Update ()
+    {
+        transform.position = target.position + distanceFromTarget;
+	}
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public Rigidbody playerBody;
+    public float speedMultiplier;
+
+    Vector3 deltaPos;
+
+    void Start ()
+    {
+        deltaPos = Vector3.zero;
+    }
+	
+	void Update ()
+    {
+        deltaPos.z = Input.GetAxis("Vertical");
+        deltaPos.x = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        playerBody.AddForce(deltaPos * speedMultiplier);
+    }
+}
