ec50dd6c52b50beaaaf5ac17f32659c95c515c9e
Mariam Tavartiladze <tavartkiladzemariam@gmail.com>
2017-05-04 20:29:48 +0400

    MenuButtons

--- Assets/Scripts/CoinCounter.cs
+++ Assets/Scripts/CoinCounter.cs
@@ -8,13 +8,35 @@ public class CoinCounter : MonoBehaviour
 {
     public int score = 0;
     public Text scoreText; //უნითიში ჩაშენებული ობიექტია
+    public string coinTagString;
+    public string badCoinTagString;
+
+    public Text resultText;
+    public string winString;
+    public string loseString;
+
+    public GameObject menu;
+    public PlayerMover myMover;
+    public Rigidbody myBody;
+
+    int TargetScore;
 
     //შემიძლია თაგების ნაცვლად გამოვიყენო ახალი ცვლადები:
     // public string coinTagString; 
     //public string badCoinTagString;
 
-   
- 
+void Start ()
+    {
+
+       GameObject [] myObjects = GameObject.FindGameObjectsWithTag(coinTagString); 
+        TargetScore= myObjects.Length;
+
+        //მეორენაირად: TargetScore = GameObject.FindGameObjectsWithTag(coinTagString).Length;
+        // ფუნქციის გამოძაახება შეიძლება ან ობიექტიდან ან კლასიდან. ის ფუნქციები რომლებიც გამოიძახებიან კლასიდან არიან სტატიკურები.
+        menu.SetActive(false);
+
+    }
+
     void OnCollisionEnter(Collision collision) // ფუნქცია, რომელიც გამოიძახება შეჯახების შემდეგ.
 
 
@@ -23,11 +45,11 @@ public class CoinCounter : MonoBehaviour
     }   
     
 	
-    void OnTriggerEnter (Collider other) // other არის სხვა ობიექტი რომელსაც ვეჯახები
+    void OnTriggerEnter (Collider other) // other არის სხვა ობიექტი რომელსაც ვეჯახები Ctrl +k Ctrl+c (comment)
 
     {
 
-        if (other.tag == "Coin")
+        if (other.tag == coinTagString)
 
         {
 
@@ -36,23 +58,56 @@ public class CoinCounter : MonoBehaviour
         }
 
 
-        else if (other.tag== "BadCoin")
+        else if (other.tag == badCoinTagString)
         {
-            if (score >0)
-            {
+            GameOver(false);
 
-                score = score - 1;
-            }
-            
+            //score = score - 1;
+
+            ////if (score >0)
+            //{
+
+            //    score = score - 1;
+            //}
+
         }
-        
 
-        Debug.Log("Triggered" +score + other.gameObject.name);
+
+        Debug.Log("Triggered " + score + " " + other.gameObject.name);
 
         scoreText.text = "Score: " + score;
         Destroy(other.gameObject); //ობიექტის განადგურება
 
+
+        if (TargetScore==score)
+
+        {
+            GameOver(true);           
+                
+                
+
+        }
     }
 
 
+    void GameOver(bool didWin)
+
+    {
+        menu.SetActive(true); //მაშინ გამოიტანოს მენიუ
+        myMover.enabled = false;
+        myBody.isKinematic = true;
+
+        if (didWin)
+        {
+
+            resultText.text = winString;
+
+        }
+
+        else
+        {
+
+            resultText.text = loseString;
+        }
+    }
 }
---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public int score;
+
+    public void RestartGame()
+
+
+    {
+
+        SceneManager.LoadScene(0);  
+
+        //Debug.Log("Restarting!");
+
+    }
+      
+	public void QuitGame()
+    {
+
+        Application.Quit();
+    }
+}
